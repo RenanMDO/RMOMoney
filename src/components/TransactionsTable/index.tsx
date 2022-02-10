3d255a697d550f6943db3881f96ad657a5a8bceb
Renan Montenegro de Oliveira <renan.montenegro.oliveira@gmail.com>
2022-02-10 15:59:33 +0100

    :tada:

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,46 @@
+
+import { useTransactions } from "../../hooks/useTransactions";
+
+import { Container } from "./styles";
+
+
+
+export function TransactionsTable() {
+  const { transactions } = useTransactions()
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Amount</th>
+            <th>Category</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('de-DE', {
+                  style: 'currency',
+                  currency: 'EUR',
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('de-DE').format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          )
+          )}
+        </tbody>
+      </table>
+    </Container>
+  );
+}
